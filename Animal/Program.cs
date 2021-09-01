31acdc67e10002506e1268341c0a791329c967e4
[ShubhamPV] <86839996+shubhamvadnere5@users.noreply.github.com>
2021-09-01 19:10:45 +0530

    AnimalProject

---
+++ Animal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    {
+        string AnimalName = "Dog";
+        string color = "White";
+
+        public void Sound()
+        {
+            Console.WriteLine("Bow Bow");
+        }
+        static void Main(string[] args)
+        {
+            Animal MyObj = new Animal();
+            Console.WriteLine(MyObj.AnimalName);
+            Console.WriteLine(MyObj.color);
+            MyObj.Sound();
+            Console.ReadLine();
+        }
+    }
+}
